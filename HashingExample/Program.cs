f06e2fb03fc41f37976c5c464f1a304683db699b
Marcus <marcus.henriksson@restory.ai>
2024-08-13 14:21:00 +0200

    Add project files.

---
+++ HashingExample/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace HashingExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+
+            //String strInput = "Hello World";
+
+            //Scanner scan = new Scanner(Systen.in);
+
+            Console.Write("Skriv in din text: ");
+            String strInput = Console.ReadLine();
+
+            //Hackning. Detta simulerar att något okänt händer med data
+            strInput += "123";
+
+            Console.WriteLine(strInput);
+
+            //Skapa ett objekt av SHA512 klassen
+            SHA512 sha = SHA512.Create();
+
+            //Hasha data, från plain text till Hexadecimalt (Byte Array)
+            byte[] hash = sha.ComputeHash(Encoding.UTF8.GetBytes(strInput));
+
+            //Konvertera ByteArray till String värde utan bindessträck mellan värden
+            string hex = BitConverter.ToString(hash).Replace("-", "");
+
+            Console.WriteLine(BitConverter.ToString(hash));
+            Console.WriteLine(hex);
+        }
+    }
+}
